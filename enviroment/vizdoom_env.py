871f9c0f633416f0e42df2abb2de952691e41a03
Paul Zintl <p.zintl@oth-aw.de>
2026-03-28 20:39:26 +0100

    renders screen and does random action

--- enviroment/vizdoom_env.py
+++ enviroment/vizdoom_env.py
@@ -3,10 +3,6 @@ import vizdoom as vzd
 import numpy as np
 from gymnasium import spaces
 
-#https://gymnasium.farama.org/index.html 
-#https://gymnasium.farama.org/introduction/create_custom_env/
-#Minimalste env für viz Deathmatch
-
 class VizDoomDeathmatchEnv(gym.Env):
     def __init__(self):
         super().__init__()
@@ -16,5 +12,46 @@ class VizDoomDeathmatchEnv(gym.Env):
         self.game.set_window_visible(True)
         self.game.init()
 
-        #missing actions, obs, step, render, reset
-        #ohne die kann weder observiert noch gestept (simuliert) werden
+        # Anzahl Actions aus VizDoom
+        n = self.game.get_available_buttons_size()
+
+        # Action Space (binary buttons)
+        self.action_space = spaces.MultiBinary(n)
+
+        # Observation Space (Bild)
+        self.observation_space = spaces.Box(
+            low=0,
+            high=255,
+            shape=(480, 640, 3),
+            dtype=np.uint8
+        )
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self.game.new_episode()
+        state = self.game.get_state()
+
+        if state is None:
+            return np.zeros((480, 640, 3), dtype=np.uint8), {}
+
+        return state.screen_buffer, {}
+
+    def step(self, action):
+        reward = self.game.make_action(action.tolist())
+
+        done = self.game.is_episode_finished()
+
+        if not done:
+            state = self.game.get_state()
+            obs = state.screen_buffer
+        else:
+            obs = np.zeros((480, 640, 3), dtype=np.uint8)
+
+        return obs, reward, done, False, {}
+
+    def render(self):
+        pass  # VizDoom rendert selbst
+
+    def close(self):
+        self.game.close()
--- runs/test_deathmatch_env.py
+++ runs/test_deathmatch_env.py
@@ -1,2 +1,16 @@
 from enviroment.vizdoom_env import VizDoomDeathmatchEnv
+import time
+
 env = VizDoomDeathmatchEnv()
+
+obs, info = env.reset()
+
+done = False
+while not done:
+    action = env.action_space.sample()  # zufällige Aktion
+    obs, reward, done, truncated, info = env.step(action)
+    
+    # Frame sichtbar halten
+    time.sleep(0.03)  # ca. 30 FPS
+
+env.close()
